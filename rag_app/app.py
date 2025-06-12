4da169074d2067da6d34b91cdf7a438ca3827d7a
Alaa hussien <95131001+Alaahussen@users.noreply.github.com>
2025-06-12 21:00:45 +0300

    Update app.py

--- rag_app/app.py
+++ rag_app/app.py
@@ -283,16 +283,16 @@ if page == "🏆 Rank Candidates":
 
                     candidates_to_process = ranked_candidates if ranked_candidates else candidate_names
                     
-                    for candidate in candidates_to_process:
-                        clean_candidate = candidate.replace('**', '')
-                        cv_path=os.path.join(upload_dir, clean_candidate + ".pdf")
-                        st.session_state.out[clean_candidate] = cv_path
-                        name_parts = extract_name_spacy(clean_candidate).lower().split()
+                    for candidate in candidate_names:
+                        cv_path = os.path.join(upload_dir, candidate + ".pdf")
+                        st.session_state.out[candidate] = cv_path
+                        name_parts = extract_name_spacy(candidate).lower().split()
                         if name_parts:
                             st.session_state.first_name_dict[name_parts[0]] = cv_path
                             if len(name_parts) > 1:
                                 full_name = " ".join(name_parts[:2])
                                 st.session_state.full_name_dict[full_name] = cv_path
+                    
                     
                     st.subheader("🏅 Top Candidates")
                     st.markdown(ranking)
