37096ca6fe93a1e85d9a28e17158cf5fdc49e07a
Alaa hussien <95131001+Alaahussen@users.noreply.github.com>
2025-06-12 22:10:51 +0300

    Update loaders.py

--- rag_app/loaders.py
+++ rag_app/loaders.py
@@ -1,5 +1,5 @@
 import os
-from langchain.document_loaders import PyMuPDFLoader, PDFPlumberLoader
+from langchain_community.document_loaders import PyMuPDFLoader, PDFPlumberLoader
 from langchain.schema import Document
 
 def load_documents(directory_path):
