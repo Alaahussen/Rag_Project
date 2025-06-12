69efea0fcc61b7f3a6c470d99286f5dbda6cbc4e
Alaa hussien <95131001+Alaahussen@users.noreply.github.com>
2025-06-12 19:35:52 +0300

    Update gemini_generate.py

--- rag_app/gemini_generate.py
+++ rag_app/gemini_generate.py
@@ -2,7 +2,7 @@ import google.generativeai as genai
 from config import GOOGLE_API_KEY
 
 def generate_response(prompt):
-    genai.configure(api_key="AIzaSyCEH5R4yYziJZlRmpClwLE7FGTrlvnUCdk")
+    genai.configure(api_key=GOOGLE_API_KEY)
     model = genai.GenerativeModel("gemini-1.5-flash")
     response = model.generate_content(prompt)
     return response.text
