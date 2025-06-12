25973af989c14f2c65750928b0f9917a6979785c
Alaa hussien <95131001+Alaahussen@users.noreply.github.com>
2025-06-12 19:25:00 +0300

    Update gemini_generate.py

--- rag_app/gemini_generate.py
+++ rag_app/gemini_generate.py
@@ -1,5 +1,5 @@
 import google.generativeai as genai
-#from config import GOOGLE_API_KEY
+from config import GOOGLE_API_KEY
 
 def generate_response(prompt):
     genai.configure(api_key=GOOGLE_API_KEY)
